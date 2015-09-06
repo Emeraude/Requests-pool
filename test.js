75c71a69aa3e275fcac6df8e145f0116fe9dfd98
Thibaut Broggi <broggi_t@epitech.eu>
2015-09-06 12:58:16 +0200

    requests are now queued + test improve
    	 test now make several requests

--- test.js
+++ test.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
 var Rp = require('./lib');
-var rp = new Rp();
+var rp = new Rp(1);
+var ids = [1337, 1338];
 
-console.log(rp.current + '/' + rp.max);
-rp.query({host: 'broggit.me', path: '/quote/1337', port: 3001, method: 'GET'}, function(e, res) {
-  if (res.statusCode != 200) {
-    console.error(res.statusCode + ': ' + res.statusMessage);
-    process.exit(1);
-  }
-  var quote = '';
-  res.on('data', function(d) {
-    quote += d;
-  }).on('end', function() {
-    console.log(quote);
+for (i in ids) {
+  console.log(rp.current + '/' + rp.max);
+  rp.query({host: 'broggit.me', path: '/quote/' + ids[i], port: 3001, method: 'GET'}, function(e, res) {
+    if (res.statusCode != 200) {
+      console.error(res.statusCode + ': ' + res.statusMessage);
+      process.exit(1);
+    }
+    var quote = '';
+    res.on('data', function(d) {
+      quote += d;
+    }).on('end', function() {
+      console.log(quote);
+    });
+    console.log(rp.current + '/' + rp.max);
   });
   console.log(rp.current + '/' + rp.max);
-});
-console.log(rp.current + '/' + rp.max);
+}
